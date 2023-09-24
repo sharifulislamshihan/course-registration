19f1caad66b4b0fcfe74a7c80843985ba89af877
sharifulislamshihan <sharifulislamshihan@gmail.com>
2023-09-25 02:58:04 +0600

    Added json and nav

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-4xl font-bold text-center my-9'>Course Registration</h1>
+    </>
+  )
+}
+
+export default App
