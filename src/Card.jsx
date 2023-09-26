30daa9b8d01f35f0e2098fe71cd9b8aeee221df4
sharifulislamshihan <sharifulislamshihan@gmail.com>
2023-09-26 13:45:17 +0600

    handled selected course

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,34 +1,51 @@
 import { useEffect } from "react";
 import Cart from "./Cart";
+import { useState } from "react";
 const Card = () => {
 
-    useEffect( () =>{
+    const [course, setCourse] = useState([]);
+    const [selectedCourse, setSelectedCourse] = useState([]);
+
+    useEffect(() => {
         fetch("../public/info.json")
-        .then((res) => res.json())
-        .then((data) => console.log(data));
-    }, [])
+            .then((res) => res.json())
+            .then((data) => setCourse(data));
+    }, []);
+
+    const handleSelectedCourse = (course) =>{
+        setSelectedCourse([...selectedCourse, course]);
+    };
+    console.log(selectedCourse);
 
 
     return (
-        <div>
-            <div id='card' className='w-72 h-96 border-solid border-2 border-orange-500 p-3'>
-                <img className='pb-4' src="https://i.ibb.co/B40B7Fw/Rectangle-2-2.png" alt="Image" />
-                <h2 className=' text-base font-bold pb-4'>Introduction to C Programming</h2>
-                <p className=' text-slate-700 pb-4 '><small>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</small></p>
+        <div id="Container" className="flex flex-col lg:flex-row mx-auto">
+            <div className="lg:w-2/3 gap-7 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 ">
+                {
+                    course.map((course) => (
+                    <div key={course.id} className='w-72 h-96 p-3'>
+                        <img className='pb-4' src={course.image} alt="Image" />
+                        <h2 className=' text-base font-bold pb-4'>{course.courseName}</h2>
+                        <p className=' text-slate-600 pb-4 '><small>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</small></p>
 
-                <div className='flex gap-7 '>
-                    <div className='flex gap-1' >
-                        <img className=' w-4 h-5 ' src="../Asset/dollar-sign 1.png" alt="" />
-                        <p className=' text-sm text-slate-700'>Price : 14000</p>
-                    </div>
+                        <div className='flex gap-7 '>
+                            <div className='flex gap-1' >
+                                <img className=' w-4 h-5 ' src="../Asset/dollar-sign 1.png" alt="" />
+                                <p className=' text-sm text-slate-600'>Price : {course.price}</p>
+                            </div>
 
-                    <div className='flex gap-1'>
-                        <img className='w-4 h-5' src="../Asset/Frame.png" alt="" />
-                        <p className=' text-sm text-slate-700'>Credit : 1hr</p>
-                    </div>
-                </div>
+                            <div className='flex gap-1'>
+                                <img className='w-4 h-5' src="../Asset/Frame.png" alt="" />
+                                <p className=' text-sm text-slate-600'>Credit : {course.credit}</p>
+                            </div>
+                        </div>
 
-                <button className=' flex w-64 h-8 bg-blue-500 rounded-lg text-white text-base font-bold my-5 justify-center items-center '>Select</button>
+                        <button onClick={()=> handleSelectedCourse(course)} className=' card-btn flex w-64 h-8 bg-blue-500 rounded-lg text-white text-base font-bold my-5 justify-center items-center '>Select</button>
+                    </div>))
+                }
+            </div>
+            <div className=" mx-auto ">
+                <Cart selectedCourse= {selectedCourse}></Cart>
             </div>
         </div>
     );
--- src/Cart.jsx
+++ src/Cart.jsx
@@ -1,7 +1,22 @@
 
-const Cart = () => {
+const Cart = ({selectedCourse}) => {
+    console.log(selectedCourse);
     return (
         <div>
+            <h2 className=" text-blue-500 text-xl font-bold my-8 pb-2 ">Credit Hour Remaining <span>20</span> hr</h2>
+
+            
+            <h2 className=" text-xl font-bold text-black my-4 ">Course Name</h2>
+
+            <ol className="list-decimal text-slate-500 text-lg font-medium ">
+                <li>jdfh</li>
+                <li>jdfh</li>
+                <li>jdfh</li>
+            </ol>
+
+            <h3 className=" my-10 text-base font-semibold ">Total Credit Hour : <span>13</span></h3>
+
+            <h3 className=" my-10 text-base font-semibold ">Total Price : <span>48000</span> USD</h3>
         </div>
     );
 };
